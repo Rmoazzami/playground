267f030bbf281d584e15af431575e3821550badb
Ryan Moazzami <ryan.moazz@gmail.com>
2020-05-16 11:51:44 -0400

    testing some mapping stuff in reactjs

--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function Shop() {
+
+    useEffect(() => {
+        fetchItems();
+        }, []);
+
+    const [items, setItems] = useState([]);
+
+    const fetchItems = async() => {
+        const data = await fetch(
+            'https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get'
+        );
+
+        // const items = await data.json();
+        const items = [{name: 'Thundergun'}, {name: "Raygun"}, {name: "HAMR"}];
+        console.log(items);
+        setItems(items);
+    }
   return (
     <div >
-      <h1>shop</h1>
+        {items.map(item => (
+            <h1>{item.name}</h1>
+        ))}
     </div>
   );
 }
